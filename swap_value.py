6463d5a518473b9cd942ee7e112fb994190579cb
AnshuAlawa <alawapappu@gmail.com>
2023-05-17 10:46:22 +0530

    swap value programm

---
+++ swap_value.py
@@ -0,0 +1,32 @@
+# first mathod
+x = 5
+y = 10
+
+print('Before swap ')
+print('x = ',x)
+print('y = ',y)
+
+z=x
+x=y
+y=z
+
+
+print('After swap ')
+print('x = ',x)
+print('y = ',y)
+
+# second mathod
+print()
+print()
+print('Second method')
+a=20
+b=50
+print('Before swap ')
+print('a = ',a)
+print('b = ',b)
+
+a,b=b,a
+
+print('After swap ')
+print('a = ',a)
+print('b = ',b)
